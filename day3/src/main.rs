9e24ea1a64e3c78fa041022b4397498fa278826f
fsadannn <fsadannn@gmail.com>
2025-12-05 18:37:27 -0500

    da3 refactor

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,13 +6,15 @@ struct MaxVal {
     value: u64,
 }
 
-fn find_max(line: &str, exclude: Option<HashSet>, skip: usize) -> MaxVal {
+fn find_max(line: &str, exclude: Option<&HashSet<usize>>, skip: usize) -> MaxVal {
     let mut d1: u64 = 0;
     let mut index: usize = 0;
+    let excluded_indexes = match exclude {
+        Some(v) => v,
+        None => &HashSet::<usize>::new(),
+    };
     for (n, &d_byte) in line.as_bytes().iter().skip(skip).enumerate() {
-        if let Some(exclude_index) = exclude
-            && exclude_index == n
-        {
+        if excluded_indexes.contains(&n) {
             continue;
         }
         let digit = (d_byte - 48) as u64;
@@ -29,12 +31,15 @@ fn main() {
     let input = shared::get_input(3);
     // part 1
     let mut sum: u64 = 0;
+    let mut set = HashSet::<usize>::with_capacity(1);
     for line in input.lines() {
         if line.is_empty() {
             continue;
         }
         let d1 = find_max(line, None, 0);
-        let d2 = find_max(line, Some(d1.index), 0);
+        set.clear();
+        set.insert(d1.index);
+        let d2 = find_max(line, Some(&set), 0);
 
         if d1.index < d2.index {
             sum += d1.value * 10 + d2.value;
